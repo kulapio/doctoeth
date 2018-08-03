096ed005581e2ce1792a8fe99575ce158b13bd40
Nattapon Nimakul <totizy@gmail.com>
2018-08-03 22:24:55 +0700

    change header word

--- src/components/SaveToEth.vue
+++ src/components/SaveToEth.vue
@@ -4,7 +4,7 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
-            Save to Ethereum
+            Save Message on Ethereum
           </h1>
           <h2 class="subtitle">
             Permanently
