54780e7d6619470a2b91b96e8306c06903b4ffba
Nattapon Nimakul <totizy@gmail.com>
2018-08-03 22:27:14 +0700

    Add Footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,20 @@
   <div class="home">
     <img src="../assets/logo.png">
     <SaveToEth/>
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import SaveToEth from '@/components/SaveToEth'
+import Footer from '@/components/Footer'
 
 export default {
   name: 'home',
   components: {
-    SaveToEth
+    SaveToEth,
+    Footer
   }
 }
 </script>
